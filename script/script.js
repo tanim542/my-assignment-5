c1b57515b0c43be22e919ec901bbb39792f65f72
tanim542 <tshahrear0@gmail.com>
2025-10-02 15:03:37 +0600

    complete all cards

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
+// function to get inner text value
+function getInnerText(id) {
+    const value = parseInt(document.getElementById(id).innerText);
+    return value;
+}
+
+// function to set innerText
+function setInnerText(value) {
+    document.getElementById("available-balance").innerText = value;
+}
+
+const heartIcons = document.getElementsByClassName("heart-icons");
+for(let heartIcon of heartIcons) {
+    heartIcon.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("heart-count").innerText = parseInt(document.getElementById("heart-count").innerText) + 1;
+    });
+}
 
