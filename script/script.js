7606095a4ffef29dd4dee0404bb61958dd6c198f
tanim542 <tshahrear0@gmail.com>
2025-10-02 23:48:18 +0600

    added functionalities in call button

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 // function to get inner text value
 function getInnerText(id) {
+    const value = document.getElementById(id).innerText;
+    return value;
+}
+function getInnerNum(id) {
     const value = parseInt(document.getElementById(id).innerText);
     return value;
 }
@@ -10,10 +14,21 @@ function setInnerText(value) {
 }
 
 const heartIcons = document.getElementsByClassName("heart-icons");
-for(let heartIcon of heartIcons) {
-    heartIcon.addEventListener("click", function(event) {
+for (let heartIcon of heartIcons) {
+    heartIcon.addEventListener("click", function (event) {
         event.preventDefault();
         document.getElementById("heart-count").innerText = parseInt(document.getElementById("heart-count").innerText) + 1;
     });
 }
 
+
+const callBtn = document.getElementsByClassName("call-btn");
+for (let btn of callBtn) {
+    btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        const card = btn.closest(".card");
+        const Num = card.querySelector(".card-num").innerText;
+        const Tittle = card.querySelector(".card-tittle").innerText;
+        alert("📞 Calling.... to " + Tittle + " " + Num);
+    });
+}
